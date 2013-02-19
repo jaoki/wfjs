6957eea03496a0e49d26ef33d89384bd5920c858
jun aoki <jun.aoki.0713@gmail.com>
2013-02-18 22:49:34 -0800

    added event

--- wfjs.js
+++ wfjs.js
@@ -17,21 +17,31 @@ var wfjs = {
 		this.svg = svg;
 
 	},
-	circle : function(label, circle_options){
+	circle : function(x, y, label, circle_options){
 		var circle = document.createElementNS(this.SVGNS, "circle");
 		for(var attr in circle_options){
 			circle.setAttribute(attr.replace("_", "-"), circle_options[attr]);
 		}
+		circle.setAttribute("cx", x);
+		circle.setAttribute("cy", y);
+		circle.addEventListener("click", this._changeColor, false);
 		this.svg.appendChild(circle);
+		
 
 		var text = document.createElementNS(this.SVGNS, "text");
 		text.textContent = label;
-		text.setAttribute("x","0");
-		text.setAttribute("y","15");
 		text.setAttribute("fill","red");
+		text.setAttribute("font-size","20");
+
 		this.svg.appendChild(text);
+		var rect = text.getBBox();
+		text.setAttribute("x", x - (rect.width/2));
+		text.setAttribute("y", y);
 
-
+	},
+	_changeColor : function(e){
+		var target = e.target;
+		target.setAttribute("fill", "purple");
 
 	}
 }
