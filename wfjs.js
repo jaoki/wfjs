c0190f2e2d33886491fcd10f9b1a4d1a7c617186
jun aoki <jun.aoki.0713@gmail.com>
2013-02-18 22:17:46 -0800

    added text

--- wfjs.js
+++ wfjs.js
@@ -2,10 +2,13 @@ var wfjs = {
 	SVGNS : "http://www.w3.org/2000/svg",
 	target: null,
 	svg: null,
+	text_default_options : {
+		fill : "black"
+	},
 	init : function(targetId){
 		this.target = document.getElementById(targetId);
 		var svg = document.createElementNS(this.SVGNS, "svg");
-		svg.setAttribute("width", "100px");
+		svg.setAttribute("width", "300px");
 		svg.setAttribute("height", "100px");
 		svg.setAttribute("id", "svg_wfjs");
 		svg.setAttribute("xmlns", this.SVGNS);
@@ -15,12 +18,21 @@ var wfjs = {
 
 
 	},
-	drawCicle : function(options){
+	circle : function(label, circle_options){
 		var circle = document.createElementNS(this.SVGNS, "circle");
-		for(var attr in options){
-			circle.setAttribute(attr.replace("_", "-"), options[attr]);
+		for(var attr in circle_options){
+			circle.setAttribute(attr.replace("_", "-"), circle_options[attr]);
 		}
 		this.svg.appendChild(circle);
+
+		var text = document.createElementNS(this.SVGNS, "text");
+		text.textContent = label;
+		text.setAttribute("x","0");
+		text.setAttribute("y","15");
+		text.setAttribute("fill","red");
+		this.svg.appendChild(text);
+
+
 
 	}
 }
