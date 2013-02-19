55cb66294ced3cd6c1f460a00a583f5289f6b0eb
jun aoki <jun.aoki.0713@gmail.com>
2013-02-18 22:04:42 -0800

    added options for circle

--- wfjs.js
+++ wfjs.js
@@ -8,29 +8,19 @@ var wfjs = {
 		svg.setAttribute("width", "100px");
 		svg.setAttribute("height", "100px");
 		svg.setAttribute("id", "svg_wfjs");
-//		svg.setAttribute("xmlns", "http://www.w3.org/2000/svg");
 		svg.setAttribute("xmlns", this.SVGNS);
 		svg.setAttribute("version", "1.1");
 		this.target.appendChild(svg);
-	   
-//	   this.target.html("<svg id='svg_wfjs' xmlns='http://www.w3.org/2000/svg' version='1.1'></svg>");
-//	   this.svg = $("#svg_wfjs");
-	   this.svg = svg;
+		this.svg = svg;
 
 
 	},
-	drawCicle : function(){
-//		var circle = document.createElement("circle");
+	drawCicle : function(options){
 		var circle = document.createElementNS(this.SVGNS, "circle");
-		circle.setAttribute("cx","100");
-		circle.setAttribute("cy","50");
-		circle.setAttribute("r","40");
-		circle.setAttribute("stroke","black");
-		circle.setAttribute("stroke-width","2");
-		circle.setAttribute("fill","red");
-
+		for(var attr in options){
+			circle.setAttribute(attr.replace("_", "-"), options[attr]);
+		}
 		this.svg.appendChild(circle);
-//		this.svg.html("<circle cx='100' cy='50' r='40' stroke='black' stroke-width='2' fill='red' />");
 
 	}
 }
