47ddb581ec7c75f4936134e90b96905459f2300e
jun aoki <junaoki@yahoo-inc.com>
2013-02-19 17:16:12 -0800

    deleted the old one

--- wfjs.js
+++ wfjs.js
@@ -11,8 +11,8 @@ var TEXT_DEFAULT_OPTIONS = {
 var draggingTarget = null;
 
 var _onMouseDown = function(e){
-	var target = e.target;
 	draggingTarget = e.target;
+	e.preventDefault();
 };
 
 
@@ -22,8 +22,6 @@ wfjs1.Canvas = (function () {
 		var _this = this;
 		var _onMouseUp = function(e){
 			if(draggingTarget != null){
-//				draggingTarget.setAttribute("cx", e.pageX);
-//				draggingTarget.setAttribute("cy", e.pageY);
 				var targetId = draggingTarget.getAttribute("id");
 				var node = _this.nodes.getById(targetId);
 				node.move(e.offsetX, e.offsetY);
@@ -126,107 +124,3 @@ return wfjs1;
 })();
 
 
-
-
-
-
-
-var wfjs10 = {
-	_wfjs : null,
-	SVGNS : "http://www.w3.org/2000/svg",
-	target: null,
-	svg: null,
-
-	text_default_options : {
-		fill : "black",
-		"font-size" : "1em",
-	},
-
-	circles : [],
-
-	init : function(targetId){
-		_wfjs = this;
-		this.target = document.getElementById(targetId);
-		var svg = document.createElementNS(this.SVGNS, "svg");
-		svg.setAttribute("id", "wfjs_svg");
-		svg.setAttribute("xmlns", this.SVGNS);
-		svg.setAttribute("version", "1.1");
-		this.target.appendChild(svg);
-		this.svg = svg;
-		svg.addEventListener("mouseup", this._onMouseUp, false);
-
-		this.circles.getById = function(id){
-			for(var i = 0; i < this.length; i++){
-				if(this[i].id == id){
-					return this[i];
-				}
-			}
-		}
-
-	},
-
-	circle : function(x, y, label, circle_options, text_options){
-		var circleId = "wfjs_circle_" + this.circles.length;
-		var textId = "wfjs_circle_" + this.circles.length + "_text";
-
-		var circle = document.createElementNS(this.SVGNS, "circle");
-		circle.setAttribute("id", circleId);
-
-		for(var attr in circle_options){
-			circle.setAttribute(attr, circle_options[attr]);
-		}
-		circle.setAttribute("cx", x);
-		circle.setAttribute("cy", y);
-		circle.addEventListener("mousedown", this._onMouseDown, false);
-		this.svg.appendChild(circle);
-
-		var text = document.createElementNS(this.SVGNS, "text");
-
-		text.setAttribute("id", textId);
-		text.textContent = label;
-
-		for(var attr in this.text_default_options){
-			text.setAttribute(attr, this.text_default_options[attr]);
-		}
-
-		if(text_options !=null && text_options !== undefined){
-			for(var attr in text_options){
-				text.setAttribute(attr, text_options[attr]);
-			}
-		}
-
-		this.svg.appendChild(text);
-		var rect = text.getBBox();
-		text.setAttribute("x", x - (rect.width/2));
-		text.setAttribute("y", y);
-
-		this.circles.push({
-			id : circleId,
-			children : [text]
-		});
-	},
-
-	draggingTarget : null,
-
-	_onMouseDown : function(e){
-		var target = e.target;
-		draggingTarget = e.target;
-	},
-
-	_onMouseUp : function(e){
-		if(draggingTarget != null){
-			draggingTarget.setAttribute("cx", e.pageX);
-			draggingTarget.setAttribute("cy", e.pageY);
-			var targetId = draggingTarget.getAttribute("id");
-			var children = _wfjs.circles.getById(targetId).children;
-			for(var i = 0; i < children.length; i++){
-				var text  = children[i];
-				var rect = text.getBBox();
-				text.setAttribute("x", e.pageX - (rect.width/2));
-				text.setAttribute("y", e.pageY);
-			}
-			draggingTarget = null;
-		}
-	},
-
-}
