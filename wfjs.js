d686b393e35de975211144d8e9d04396ad4fea6a
jun aoki <junaoki@yahoo-inc.com>
2013-02-19 20:15:15 -0800

    line end on the edige

--- wfjs.js
+++ wfjs.js
@@ -126,23 +126,13 @@ wfjs1.Node = (function () {
 		lineElement.setAttribute("y1", this.y);
 
 		// The line end should not be the center of the target object, since it needs an arrow mark.
-		var radian = Math.atan2(node.y - this.y, node.x - this.x);
-
+//		var radian = Math.atan2(node.y - this.y, node.x - this.x);
+		var radian = Math.atan2(this.y - node.y, this.x - node.x);
 
 		var endX = node.circle_options.r * Math.cos(radian) + node.x;
-//		if(this.x < node.x){
-//			endX = node.x - node.circle_options.r;
-//		}else{
-//			endX = node.x + node.circle_options.r;
-//		}
 		lineElement.setAttribute("x2", endX);
 
 		var endY = node.circle_options.r * Math.sin(radian) + node.y;
-//		if(this.y < node.y){
-//			endY = node.y - node.circle_options.r;
-//		}else{
-//			endY = node.y + node.circle_options.r;
-//		}
 		lineElement.setAttribute("y2", endY);
 
 		lineElement.setAttribute("style", "stroke: black;stroke-width:1;");
